e46f76c311c0d8c35ca5442528bfc91142f0c260
aryandewan <aryan.dewan.05@gmail.com>
2025-03-08 12:22:50 +0530

    updates

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,6 +7,7 @@ import CompletedList from "./Completed.jsx";
 import { FaSearch } from "react-icons/fa";
 import { PiStarFourFill } from "react-icons/pi";
 import gsap from "gsap";
+import { Toaster, toast } from "react-hot-toast";
 
 const App = () => {
   const [topTenAnime, setTopTenAnime] = useState([]);
@@ -165,6 +166,14 @@ const App = () => {
       setWatchList(updatedWatchList);
       localStorage.setItem("watchList", JSON.stringify(updatedWatchList));
     }
+    toast.success("Added to Watch List", {
+      duration: 1000,
+      position: "center-center",
+      style: {
+        background: "#333",
+        color: "#fff",
+      },
+    });
   };
 
   const addToWatchingList = (anime) => {
@@ -173,6 +182,14 @@ const App = () => {
       setWatchingList(updatedWatchList);
       localStorage.setItem("watchingList", JSON.stringify(updatedWatchList));
     }
+    toast.success("Added to Watching List", {
+      duration: 1000,
+      position: "center-center",
+      style: {
+        background: "#333",
+        color: "#fff",
+      },
+    });
   };
 
   const addToCompletedList = (anime) => {
@@ -186,12 +203,29 @@ const App = () => {
     }
 
     removeFromWatchingList(anime);
+
+    toast.success("Added to Completed List", {
+      duration: 1000,
+      position: "center-center",
+      style: {
+        background: "#333",
+        color: "#fff",
+      },
+    });
   };
 
   const removeFromWatchList = (anime) => {
     const updatedWatchList = watchList.filter((a) => a.mal_id !== anime.mal_id);
     setWatchList(updatedWatchList);
     localStorage.setItem("watchList", JSON.stringify(updatedWatchList));
+    toast.success("Removed from Watch List", {
+      duration: 1000,
+      position: "center-center",
+      style: {
+        background: "#333",
+        color: "#fff",
+      },
+    });
   };
 
   const removeFromCompletedList = (anime) => {
@@ -200,6 +234,14 @@ const App = () => {
     );
     setCompletedList(updatedCompletedList);
     localStorage.setItem("completedList", JSON.stringify(updatedCompletedList));
+    toast.success("Removed from Completed List", {
+      duration: 1000,
+      position: "center-center",
+      style: {
+        background: "#333",
+        color: "#fff",
+      },
+    });
   };
 
   const removeFromWatchingList = (anime) => {
@@ -208,171 +250,182 @@ const App = () => {
     );
     setWatchingList(updatedWatchingList);
     localStorage.setItem("watchingList", JSON.stringify(updatedWatchingList));
+    toast.success("Removed from Watching List", {
+      duration: 1000,
+      position: "center-center",
+      style: {
+        background: "#333",
+        color: "#fff",
+      },
+    });
   };
 
   return (
-    <main className="flex flex-col items-center justify-center overflow-y-auto">
-      <div className="relative w-full h-[100dvh]">
-        {isXL ? (
-          <div className="flex items-center justify-between gap-4 w-full p-5 absolute top-0 left-0 z-100">
-            <nav className="flex flex-row gap-5">
+    <>
+      <Toaster />
+      <main className="flex flex-col items-center justify-center overflow-y-auto">
+        <div className="relative w-full h-[100dvh]">
+          {isXL ? (
+            <div className="flex items-center justify-between gap-4 w-full p-5 absolute top-0 left-0 z-100">
+              <nav className="flex flex-row gap-5">
+                <h1
+                  className="text-2xl font-bold text-white cursor-pointer flex flex-row"
+                  onClick={heroCompfunc}
+                >
+                  Ani
+                  <PiStarFourFill className="text-[#8b2abf] text-4xl transform rotate-45" />
+                </h1>
+                <h1
+                  className={`text-2xl cursor-pointer font-semibold hover:text-[#8b2abf] transition-all duration-200 ${
+                    listView ? "text-[#8b2abf]" : "text-white"
+                  }`}
+                  onClick={listViewfunction}
+                >
+                  MyAnimeList
+                </h1>
+                <h1
+                  className={`text-2xl cursor-pointer font-semibold hover:text-[#8b2abf] transition-all duration-200 ${
+                    watchingListView ? "text-[#8b2abf]" : "text-white"
+                  }`}
+                  onClick={watchingListViewfunction}
+                >
+                  Watching
+                </h1>
+                <h1
+                  className={`text-2xl cursor-pointer font-semibold hover:text-[#8b2abf] transition-all duration-200 ${
+                    completedListView ? "text-[#8b2abf]" : "text-white"
+                  }`}
+                  onClick={completedListViewfunction}
+                >
+                  Completed
+                </h1>
+              </nav>
+              <form
+                action={animeSearch}
+                className="flex items-center gap-2 w-2/3 justify-between lg:w-1/3"
+              >
+                <input
+                  name="search"
+                  id="search"
+                  type="search"
+                  placeholder="Search"
+                  className="border-2 border-gray-300 rounded-full p-3 outline-none w-full text-white"
+                  autoComplete="off"
+                  required
+                />
+                <button
+                  type="submit"
+                  className="bg-[#8b2abf] text-white p-4 rounded-full hover:bg-[#45155e] transition-all duration-200"
+                >
+                  <FaSearch />
+                </button>
+              </form>
+            </div>
+          ) : (
+            <div className="flex justify-between gap-4 w-full p-5 absolute top-0 left-0 z-100">
               <h1
-                className="text-2xl font-bold text-white cursor-pointer flex flex-row"
+                className="text-2xl font-bold text-white cursor-pointer"
                 onClick={heroCompfunc}
               >
-                Ani
                 <PiStarFourFill className="text-[#8b2abf] text-4xl transform rotate-45" />
               </h1>
-              <h1
-                className={`text-2xl cursor-pointer font-semibold hover:text-[#8b2abf] transition-all duration-200 ${
-                  listView ? "text-[#8b2abf]" : "text-white"
-                }`}
-                onClick={listViewfunction}
-              >
-                MyAnimeList
-              </h1>
-              <h1
-                className={`text-2xl cursor-pointer font-semibold hover:text-[#8b2abf] transition-all duration-200 ${
-                  watchingListView ? "text-[#8b2abf]" : "text-white"
-                }`}
-                onClick={watchingListViewfunction}
-              >
-                Watching
-              </h1>
-              <h1
-                className={`text-2xl cursor-pointer font-semibold hover:text-[#8b2abf] transition-all duration-200 ${
-                  completedListView ? "text-[#8b2abf]" : "text-white"
-                }`}
-                onClick={completedListViewfunction}
-              >
-                Completed
-              </h1>
-            </nav>
-            <form
-              action={animeSearch}
-              className="flex items-center gap-2 w-2/3 justify-between lg:w-1/3"
-            >
-              <input
-                name="search"
-                id="search"
-                type="search"
-                placeholder="Search"
-                className="border-2 border-gray-300 rounded-full p-3 outline-none w-full text-white"
-                autoComplete="off"
-                required
-              />
-              <button
-                type="submit"
-                className="bg-[#8b2abf] text-white p-4 rounded-full hover:bg-[#45155e] transition-all duration-200"
-              >
-                <FaSearch />
-              </button>
-            </form>
-          </div>
-        ) : (
-          <div className="flex justify-between gap-4 w-full p-5 absolute top-0 left-0 z-100">
-            <h1
-              className="text-2xl font-bold text-white cursor-pointer"
-              onClick={heroCompfunc}
-            >
-              <PiStarFourFill className="text-[#8b2abf] text-4xl transform rotate-45" />
-            </h1>
 
-            {navAnimation ? (
-              <nav
-                ref={navRef}
-                className="flex flex-col gap-5 w-2/3 md:w-1/3 h-[95vh] fixed top-5 right-5 z-100"
-              >
-                <div className="flex flex-row gap-5 w-full justify-between">
-                  <form
-                    action={animeSearch}
-                    className="flex gap-5 justify-between w-full h-[48px]"
-                  >
-                    <input
-                      name="search"
-                      id="search"
-                      type="search"
-                      placeholder="Search"
-                      className="border-2 border-gray-300 rounded-full p-3 outline-none w-full text-white"
-                      autoComplete="off"
-                      required
-                    />
-                    <button
-                      type="submit"
-                      className="bg-[#8b2abf] text-white p-4 rounded-full hover:bg-[#45155e] transition-all duration-200"
+              {navAnimation ? (
+                <nav
+                  ref={navRef}
+                  className="flex flex-col gap-5 w-2/3 md:w-1/3 h-[95vh] fixed top-5 right-5 z-100"
+                >
+                  <div className="flex flex-row gap-5 w-full justify-between">
+                    <form
+                      action={animeSearch}
+                      className="flex gap-5 justify-between w-full h-[48px]"
                     >
-                      <FaSearch />
-                    </button>
-                  </form>
-                </div>
-                <div className="flex flex-col gap-5 h-full p-5 bg-white/10 backdrop-blur border border-white/20 shadow-lg rounded-lg">
-                  <h1
-                    className="text-xl text-white cursor-pointer font-semibold hover:text-[#8b2abf] transition-all duration-200"
-                    onClick={listViewfunction}
-                  >
-                    MyAnimeList
-                  </h1>
-                  <h1
-                    className="text-xl text-white cursor-pointer font-semibold hover:text-[#8b2abf] transition-all duration-200"
-                    onClick={watchingListViewfunction}
-                  >
-                    Watching
-                  </h1>
-                  <h1
-                    className="text-xl text-white cursor-pointer font-semibold hover:text-[#8b2abf] transition-all duration-200"
-                    onClick={completedListViewfunction}
-                  >
-                    Completed
-                  </h1>
-                </div>
-              </nav>
-            ) : (
-              <button
-                onClick={navAni}
-                className="bg-[#8b2abf] text-white p-4 rounded-full hover:bg-[#45155e] transition-all duration-200"
-              >
-                <FaSearch />
-              </button>
-            )}
-          </div>
-        )}
-        {heroComp && (
-          <Hero
-            topTenAnime={topTenAnime}
-            addToWatchList={addToWatchList}
-            addToWatchingList={addToWatchingList}
-            addToCompletedList={addToCompletedList}
-          />
-        )}
-        {search && (
-          <Search
-            anime={anime}
-            addToWatchList={addToWatchList}
-            addToCompletedList={addToCompletedList}
-            addToWatchingList={addToWatchingList}
-          />
-        )}
-        {listView && (
-          <List
-            watchList={watchList}
-            removeFromWatchList={removeFromWatchList}
-            addToWatchingList={addToWatchingList}
-          />
-        )}
-        {watchingListView && (
-          <WatchList
-            watchingList={watchingList}
-            addToCompletedList={addToCompletedList}
-          />
-        )}
-        {completedListView && (
-          <CompletedList
-            completedList={completedList}
-            removeFromCompletedList={removeFromCompletedList}
-          />
-        )}
-      </div>
-    </main>
+                      <input
+                        name="search"
+                        id="search"
+                        type="search"
+                        placeholder="Search"
+                        className="border-2 border-gray-300 rounded-full p-3 outline-none w-full text-white"
+                        autoComplete="off"
+                        required
+                      />
+                      <button
+                        type="submit"
+                        className="bg-[#8b2abf] text-white p-4 rounded-full hover:bg-[#45155e] transition-all duration-200"
+                      >
+                        <FaSearch />
+                      </button>
+                    </form>
+                  </div>
+                  <div className="flex flex-col gap-5 h-full p-5 bg-white/10 backdrop-blur border border-white/20 shadow-lg rounded-lg">
+                    <h1
+                      className="text-xl text-white cursor-pointer font-semibold hover:text-[#8b2abf] transition-all duration-200"
+                      onClick={listViewfunction}
+                    >
+                      MyAnimeList
+                    </h1>
+                    <h1
+                      className="text-xl text-white cursor-pointer font-semibold hover:text-[#8b2abf] transition-all duration-200"
+                      onClick={watchingListViewfunction}
+                    >
+                      Watching
+                    </h1>
+                    <h1
+                      className="text-xl text-white cursor-pointer font-semibold hover:text-[#8b2abf] transition-all duration-200"
+                      onClick={completedListViewfunction}
+                    >
+                      Completed
+                    </h1>
+                  </div>
+                </nav>
+              ) : (
+                <button
+                  onClick={navAni}
+                  className="bg-[#8b2abf] text-white p-4 rounded-full hover:bg-[#45155e] transition-all duration-200"
+                >
+                  <FaSearch />
+                </button>
+              )}
+            </div>
+          )}
+          {heroComp && (
+            <Hero
+              topTenAnime={topTenAnime}
+              addToWatchList={addToWatchList}
+              addToWatchingList={addToWatchingList}
+              addToCompletedList={addToCompletedList}
+            />
+          )}
+          {search && (
+            <Search
+              anime={anime}
+              addToWatchList={addToWatchList}
+              addToCompletedList={addToCompletedList}
+              addToWatchingList={addToWatchingList}
+            />
+          )}
+          {listView && (
+            <List
+              watchList={watchList}
+              removeFromWatchList={removeFromWatchList}
+              addToWatchingList={addToWatchingList}
+            />
+          )}
+          {watchingListView && (
+            <WatchList
+              watchingList={watchingList}
+              addToCompletedList={addToCompletedList}
+            />
+          )}
+          {completedListView && (
+            <CompletedList
+              completedList={completedList}
+              removeFromCompletedList={removeFromCompletedList}
+            />
+          )}
+        </div>
+      </main>
+    </>
   );
 };
 
